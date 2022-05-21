241c3973525ac0865b289354828119121abee5b8
gmakc-094423 <gmakc-094423@yandex.ru>
2022-05-21 03:40:42 +0300

    домашнее задание по 8 семинару. Уточненное

--- DZ_08-2(54)/Program.cs
+++ DZ_08-2(54)/Program.cs
@@ -21,14 +21,14 @@ void OrderArrayLines(int[,] array)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      for (int k = 0; k < array.GetLength(1)-1; k++)
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
       {
-       	if (array[i,k] < array[i,k + 1])
-					{
-						int temp = array[i,k + 1];
-						array[i,k + 1] = array[i,k];
-						array[i,k] = temp;
-					}
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
       }
     }
   }
@@ -52,13 +52,13 @@ void CreateArray(int[,] array)
   }
 }
 
-void WriteArray (int[,] array)
+void WriteArray(int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i,j] + " ");
+      Console.Write(array[i, j] + " ");
     }
     Console.WriteLine();
   }
--- DZ_08-5(60)/Program.cs
+++ DZ_08-5(60)/Program.cs
@@ -12,7 +12,6 @@ int[,,] array3D = new int[x, y, z];
 CreateArray(array3D);
 WriteArray(array3D);
 
-
 int InputNumbers(string input)
 {
   Console.Write(input);
@@ -20,7 +19,7 @@ int InputNumbers(string input)
   return output;
 }
 
-void WriteArray (int[,,] array3D)
+void WriteArray(int[,,] array3D)
 {
   for (int i = 0; i < array3D.GetLength(0); i++)
   {
@@ -29,7 +28,7 @@ void WriteArray (int[,,] array3D)
       Console.Write($"X({i}) Y({j}) ");
       for (int k = 0; k < array3D.GetLength(2); k++)
       {
-        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+        Console.Write($"Z({k})={array3D[i, j, k]}; ");
       }
       Console.WriteLine();
     }
@@ -40,7 +39,7 @@ void WriteArray (int[,,] array3D)
 void CreateArray(int[,,] array3D)
 {
   int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
-  int  number;
+  int number;
   for (int i = 0; i < temp.GetLength(0); i++)
   {
     temp[i] = new Random().Next(10, 100);
@@ -55,11 +54,11 @@ void CreateArray(int[,,] array3D)
           j = 0;
           number = temp[i];
         }
-          number = temp[i];
+        number = temp[i];
       }
     }
   }
-  int count = 0; 
+  int count = 0;
   for (int x = 0; x < array3D.GetLength(0); x++)
   {
     for (int y = 0; y < array3D.GetLength(1); y++)
